16b02a172c9e2b0a3a513685fe96e095934c04c5
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-11-21 09:00:17 +0100

    first commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Random GuessTheNumber = new Random();
+
+        int number = GuessTheNumber.Next(0, 101);
+
+        bool guessedCorrectly = false;
+
+        while (!guessedCorrectly)
+        { 
+        Console.WriteLine("Guess a number");
+        int guess = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("You guessed " + guess);
+
+        if (guess == number)
+        {
+            Console.WriteLine("You guessed wright, great job!");
+                guessedCorrectly = true;
+        }
+        else if(guess > number)
+        {
+            Console.WriteLine("Guess is too high");
+                guessedCorrectly = false;
+        }
+        else
+        {
+            Console.WriteLine("Guess is too low");
+                guessedCorrectly = false;
+        }
+    }
+}
+}
